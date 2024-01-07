2735089d56fccfc12c22a8561e17b85acb2ff6fc
Gussriquena <gustavo_riquena1@outlook.com>
2024-01-07 13:23:46 -0300

    feat(General): Project setup

---
+++ src/main/java/service/bookwallet/ServiceBookwalletApplication.java
@@ -0,0 +1,13 @@
+package service.bookwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/service/bookwallet/ServiceBookwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package service.bookwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
